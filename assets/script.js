02c099a851f23b2aeea59c4aea20e0abbec558a7
Emily McCormick <emilyannmccormick@gmail.com>
2021-05-17 14:00:01 -0500

    add hours and local storage

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Current day is displayed at top
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save in local storage
+        localStorage.setItem(time, text);
+    })
+
+    // code to change the class depending on time could go here
+   
+    // Get info from local storage 
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+
+
+})
